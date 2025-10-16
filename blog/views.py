cecfec3177ecb8fe3f86e0bb99a0f92880928f07
Gram-S <grantms4455@gmail.com>
2025-10-16 17:29:32 -0600

    broken

--- blog/views.py
+++ blog/views.py
@@ -4,8 +4,8 @@ from .models import Post
 # Create your views here.
 def post_list(request):
     posts = Post.objects.all()
-    return render(request, 'blog/home.html', {'posts': posts})
+    return render(request, 'home.html', {'posts': posts})
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, "blog/post_detail.html", {"post": post})
+    return render(request, "post_detail.html", {"post": post})
